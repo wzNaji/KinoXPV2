ba22b404c56b76bfdc93160ce7de1eddd24066a2
wzNaji <ludi2300@gmail.com>
2024-10-01 14:35:03 +0200

    Initial

---
+++ src/main/java/com/wzn/kinoxpv2/KinoXpv2Application.java
@@ -0,0 +1,13 @@
+package com.wzn.kinoxpv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoXpv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoXpv2Application.class, args);
+    }
+
+}
